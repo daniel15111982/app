9b01ae31c5f1e2a4a1144e16602241b7c3e0dc8c
System Administrator <root@MacBook-Pro-de-Daniel.local>
2015-10-02 00:20:00 -0300

    primeiro push funcinal

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,64 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var BookIt = BookIt || {};
+
+// Begin boilerplate code generated with Cordova project.
+
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function (id) {
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
 
 app.initialize();
+
+// End boilerplate code.
+
+$(document).on("mobileinit", function (event, ui) {
+    $.mobile.defaultPageTransition = "slide";
+});
+
+app.signupController = new BookIt.SignUpController();
+
+//$(document).delegate("#page-signup", "pagebeforeshow", function () {
+//    // Reset the signup form.
+//    app.signupController.resetSignUpForm();
+//});
+
+$(document).on("pagecontainerbeforeshow", function (event, ui) {
+    if (typeof ui.toPage == "object") {
+        switch (ui.toPage.attr("id")) {
+            case "page-signup":
+                // Reset the signup form.
+                app.signupController.resetSignUpForm();
+                break;
+        }
+    }
+});
+
+$(document).delegate("#page-signup", "pagebeforecreate", function () {
+
+    app.signupController.init();
+
+    app.signupController.$btnSubmit.off("tap").on("tap", function () {
+        app.signupController.onSignupCommand();
+    });
+
+});
